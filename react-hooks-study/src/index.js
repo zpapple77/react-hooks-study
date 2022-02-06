f264d6288cfd6050c1cae09f0a892e97e6e41954
Pinger <zpapple77@163.com>
2022-02-06 23:34:53 +0800

    useState的基本使用

--- react-hooks-study/src/index.js
+++ react-hooks-study/src/index.js
@@ -1,18 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import Position from './Position'
-// import Dog from './Dog'
-import withMouse from './withMouse'
-import withScroll from './withScroll'
 
+//class组件实现加一
+// class App extends React.Component {
+//   state = {
+//     count: 0,
+//   }
+//   render() {
+//     return (
+//       <div>
+//         <h1>根组件</h1>
+//         <div>点击次数:{this.state.count}</div>
+//         <button onClick={this.add}>+1</button>
+//       </div>
+//     )
+//   }
+//   add = () => {
+//       this.setState({
+//           count:this.state.count+1,
+//       })
+//   }
+// }
+// ReactDOM.render(<App />, document.getElementById('root'))
 
-const P = withMouse(withScroll(Position))
-const element = (
-  <div style={{width:1000,height:1000}}>
-    <h1>根组件</h1>
-    {/* <DogWithMouse></DogWithMouse> */}
-    <P></P>
-  </div>
-)
+//使用hook实现---useState()
+function App() {
+    //useState()参数：初始值
+    //返回值：就是一个数组，长度为2
+    //数组下标0：就是这个状态  数组下标1：修改这个状态的函数
+ const [count,setCount]=useState(0)//解构
+//  const count = res[0]
+//  const setCount = res[1]
+  return (
+    <div>
+      <h1>根组件</h1>
+      <div>点击次数:{count}</div>
+      <button onClick={()=>{setCount(count+1)}}>+1</button>
+    </div>
+  )
+}
 
-ReactDOM.render(element, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
