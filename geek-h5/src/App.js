699261ef62b29aec4df9990e607c8b801242552a
Pinger <zpapple77@163.com>
2022-02-15 20:44:37 +0800

    空

--- geek-h5/src/App.js
+++ geek-h5/src/App.js
@@ -1,5 +1,25 @@
+import React, { Suspense } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+import './App.scss'
+const Login = React.lazy(() => import('@/pages/Login'))
+const Home = React.lazy(() => import('@/pages/Home'))
 export default function App() {
   return (
-    <div>App</div>
+    <Router>
+      <div className="app">
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Redirect exact from="/" to="/home"></Redirect>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
   )
 }
--- geek-h5/src/index.js
+++ geek-h5/src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+// 导入通用样式
+import '@scss/index.scss'
 
 ReactDOM.render(<App />, document.getElementById('root'))
