0c03fcc8a8cc7768ce2a9840360f39002206f135
Pinger <zpapple77@163.com>
2022-02-19 12:31:05 +0800

    登入功能-icon组件的封装

--- geek-m/src/App.js
+++ geek-m/src/App.js
@@ -1,13 +1,27 @@
-import {Button, Toast} from 'antd-mobile'
-import './index.scss'
+import React, { Suspense } from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom'
+const Login = React.lazy(() => import('@/pages/Login'))
+const Home = React.lazy(() => import('@/pages/Home'))
+
 export default function App() {
-  const test = () => {
-    Toast.success('Load success!!!',1)
-  }
   return (
-    <div>
-       <Button type="primary" onClick={test}>primary</Button>
-       <div className="box">aaa</div>
-    </div>
+    <Router>
+      <div className="app">
+        {/* <Link to="/login">登录</Link>
+        <Link to="/home">首页</Link> */}
+        <Suspense fallback={<div>loading...</div>}>
+          <Switch>
+            <Redirect exact from="/" to="/home"></Redirect>
+            <Route path="/login" component={Login}></Route>
+            <Route path="/home" component={Home}></Route>
+          </Switch>
+        </Suspense>
+      </div>
+    </Router>
   )
 }
---
+++ geek-m/src/pages/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Icon from '@/components/icon'
+import './index.scss'
+export default function Login() {
+  return (
+    <div>
+      <h1>我是登入页面</h1>
+      <Icon type="icon-mianxingfeizhunan" className="big"></Icon>
+    </div>
+  )
+}
