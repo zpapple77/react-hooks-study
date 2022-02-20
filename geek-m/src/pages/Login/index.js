ee4f43a60ba390e4a6e9c7e47a1d8782e04b172e
Pinger <zpapple77@163.com>
2022-02-20 21:11:54 +0800

    登入功能-input组件的封装

--- geek-m/src/pages/Login/index.js
+++ geek-m/src/pages/Login/index.js
@@ -1,6 +1,10 @@
 import NavBar from '@/components/NavBar'
 import styles from './index.module.scss'
+import Input from '@/components/Input'
 export default function Login() {
+  const onExtraClick = () => {
+    console.log("aaa");
+  }
   return (
     <div className={styles.root}>
       {/* 标题 */}
@@ -10,15 +14,19 @@ export default function Login() {
         <h3>短信登入</h3>
         <form>
           <div className="input-item">
-            <input type="text" />
-            <div className="validate">手机号验证错误信息</div>
+            <Input placeholder="请输入手机号"></Input>
+            {/* <div className="validate">手机号验证错误信息</div> */}
           </div>
           <div className="input-item">
-            <input type="text" />
-            <div className="validate">验证码验证错误信息</div>
+            <Input
+              placeholder="请输入验证码"
+              extra="获取验证码"
+              onExtraClick={onExtraClick}
+            ></Input>
+            {/* <div className="validate">验证码验证错误信息</div> */}
           </div>
-           {/* 登录按钮 */}
-           <button type="submit" className="login-btn">
+          {/* 登录按钮 */}
+          <button type="submit" className="login-btn">
             登录
           </button>
         </form>
