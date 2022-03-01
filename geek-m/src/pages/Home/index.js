bd014a71107caaf7b1f5a4600049732bcac367ac
Pinger <zpapple77@163.com>
2022-03-02 00:00:48 +0800

    首页-频道渲染

--- geek-m/src/pages/Home/index.js
+++ geek-m/src/pages/Home/index.js
@@ -1,35 +1,14 @@
 import React, { useEffect } from 'react'
 import styles from './index.module.scss'
 import Tabs from '@/components/Tabs'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getUserChannels } from '@/store/actions/home'
 export default function Home() {
   const dispatch = useDispatch()
-  useEffect(()=>{
+  useEffect(() => {
     dispatch(getUserChannels())
-  },[dispatch])
-  const tabs = [
-    {
-      id: 1,
-      name: '频道1',
-    },
-    {
-      id: 2,
-      name: '频道2',
-    },
-    {
-      id: 3,
-      name: '频道3',
-    },
-    {
-      id:4,
-      name: '频道4',
-    },
-    {
-      id: 5,
-      name: '频道5',
-    },
-  ]
+  }, [dispatch])
+  const tabs = useSelector((state) => state.home.userChannels)
   return (
     <div className={styles.root}>
       <Tabs tabs={tabs}></Tabs>
